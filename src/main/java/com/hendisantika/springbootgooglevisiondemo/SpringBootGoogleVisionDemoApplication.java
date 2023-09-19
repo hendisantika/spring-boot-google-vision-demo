292ae98334d4020b24037aacf0760d64b84c223e
Hendi Santika <hendisantika@yahoo.co.id>
2023-09-20 05:52:36 +0700

    #1 - Init project ✏️📗:octocat:⬆

---
+++ src/main/java/com/hendisantika/springbootgooglevisiondemo/SpringBootGoogleVisionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootgooglevisiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGoogleVisionDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGoogleVisionDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootgooglevisiondemo/SpringBootGoogleVisionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootgooglevisiondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGoogleVisionDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
